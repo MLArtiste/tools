2ecf6a018db71942604d714c944c495dd0e652e0
MLA <mlartiste@proton.me>
2026-05-08 14:49:15 +0100

    upload v0.6.1 to pypi

--- mlalib/__init__.py
+++ mlalib/__init__.py
@@ -4,7 +4,7 @@ For the love of exploring intelligence.
 
 from . import utils, tabular, text
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 __author__ = "MLA"
 
 __all__ = ["tabular", "utils", "text"]
