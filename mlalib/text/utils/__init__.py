35f5e53d5b6ba888a94952ecf74193bb830a657a
MLA <mlartiste@proton.me>
2026-06-24 23:37:13 +0100

    refactor: reorder imports and update __all__ to include BPE

--- mlalib/text/utils/__init__.py
+++ mlalib/text/utils/__init__.py
@@ -1,14 +1,14 @@
 from ._utils import (
-    CharTokenizer,
-    Tokenizer,
-    Vocab,
-    WordTokenizer,
     build_counter_from_iterator,
     build_vocab_from_iterator,
     ngrams_iterator,
 )
+from ._vocab import Vocab
+from ._tokenizer import CharTokenizer, Tokenizer, WordTokenizer
+from ._bpe import BPE
 
 __all__ = [
+    "BPE",
     "CharTokenizer",
     "Tokenizer",
     "Vocab",
