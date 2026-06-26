d7ae7b0b1c36500ad177880818184abdc438c1f5
MLA <mlartiste@proton.me>
2026-06-26 22:51:44 +0100

    bump version to 0.6.4 for pypi

--- mlalib/__init__.py
+++ mlalib/__init__.py
@@ -4,7 +4,7 @@ For the love of exploring intelligence.
 
 from . import utils, tabular, text
 
-__version__ = "0.6.3"
+__version__ = "0.6.4"
 __author__ = "MLA"
 
 __all__ = ["tabular", "utils", "text"]
